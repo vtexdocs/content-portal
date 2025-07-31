f52e225b326b5b1d8bac7418bf3f3089f6f2da84
GeorgeLimaDev <george.lima@vtex.com.br>
2025-07-31 09:36:45 -0300

    WIP: first functional version of glossary table with mocked data

--- src/pages/docs/glossary/index.tsx
+++ src/pages/docs/glossary/index.tsx
@@ -1,16 +1,23 @@
-import { Box, Flex, Text } from '@vtex/brand-ui'
 import PageHeader from 'components/page-header'
-import WhatsNextCard from 'components/whats-next-card'
 import { GetStaticProps, NextPage } from 'next'
 import Head from 'next/head'
-import { Fragment, useContext } from 'react'
+import Script from 'next/script'
+import { Fragment, useContext, useEffect, useState } from 'react'
 import { useIntl } from 'react-intl'
-import styles from 'styles/documentation-landing-page'
 import { PreviewContext } from 'utils/contexts/preview'
 import getNavigation from 'utils/getNavigation'
 import { DocumentationTitle, UpdatesTitle } from 'utils/typings/unionTypes'
 import GrammarImage from '../../../../public/images/cs-grammar_desktop.png'
 import GrammarImageMobile from '../../../../public/images/cs-grammar_mobile.png'
+import styles from './glossary.module.css'
+import { glossaryData } from './glossaryData'
+
+declare global {
+  interface Window {
+    jQuery: any
+    DataTable: any
+  }
+}
 
 interface Props {
   sidebarfallback: any //eslint-disable-line
@@ -18,46 +25,42 @@ interface Props {
   branch: string
 }
 
-const ContentSection = ({ id, length }: { id: string; length: number }) => {
-  const intl = useIntl()
-
-  return (
-    <>
-      <Text sx={styles.contentTitle}>
-        {intl.formatMessage({ id: `${id}.title` })}
-      </Text>
-      <Flex sx={styles.cardsContainer}>
-        {Array(length)
-          .fill('')
-          .map((_, index) => {
-            if (!intl.messages[`${id}.content.${index}.title`]) return <></>
-            return (
-              <WhatsNextCard
-                title={intl.formatMessage({
-                  id: `${id}.content.${index}.title`,
-                })}
-                description={intl.formatMessage({
-                  id: `${id}.content.${index}.description`,
-                })}
-                linkTitle={intl.formatMessage({
-                  id: 'start_here_see_more',
-                })}
-                linkTo={intl.formatMessage({
-                  id: `${id}.content.${index}.link`,
-                })}
-                key={intl.formatMessage({ id: `${id}.content.${index}.title` })}
-              />
-            )
-          })}
-      </Flex>
-    </>
-  )
-}
 
 const TracksPage: NextPage<Props> = ({ branch }) => {
   const { setBranchPreview } = useContext(PreviewContext)
   const intl = useIntl()
+  const [dataTable, setDataTable] = useState<any>(null)
+
   setBranchPreview(branch)
+
+  const handleScriptLoad = () => {
+    if (typeof window.jQuery === 'function' && !dataTable) {
+      const table = window.jQuery('#glossaryTable').DataTable({
+        destroy: true,
+        language: {
+          search: 'Search:',
+          lengthMenu: 'View _MENU_ items per page',
+          info: 'Viewing from _START_ to _END_ of _TOTAL_ items',
+          paginate: {
+            first: 'First',
+            last: 'Last',
+            next: 'Next',
+            previous: 'Previous',
+          },
+        },
+      })
+      setDataTable(table)
+    }
+  }
+
+  useEffect(() => {
+    return () => {
+      if (dataTable) {
+        dataTable.destroy()
+      }
+    }
+  }, [dataTable])
+
   return (
     <>
       <Head>
@@ -72,6 +75,10 @@ const TracksPage: NextPage<Props> = ({ branch }) => {
             id: 'glossary_card_subtitle',
           })}
           key="title"
+        />
+        <link
+          rel="stylesheet"
+          href="https://cdn.datatables.net/2.3.2/css/dataTables.dataTables.css"
         />
       </Head>
       <Fragment>
@@ -88,10 +95,42 @@ const TracksPage: NextPage<Props> = ({ branch }) => {
             id: 'glossary_card_subtitle',
           })}
         />
-        {/* <Box sx={styles.contentContainer}>
-          <ContentSection id={'grammar_page_section'} length={9} />
-        </Box> */}
+
+        <div className={styles.dataTableContainer}>
+          <div className="content">
+            <table
+              id="glossaryTable"
+              className="display"
+              style={{ width: '100%' }}
+            >
+              <thead>
+                <tr>
+                  <th>Term</th>
+                  <th>Definition</th>
+                </tr>
+              </thead>
+              <tbody>
+                {glossaryData.map((item, index) => (
+                  <tr key={index}>
+                    <td>{item.term}</td>
+                    <td>{item.definition}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
       </Fragment>
+
+      <Script
+        src="https://code.jquery.com/jquery-3.7.1.min.js"
+        strategy="afterInteractive"
+      />
+      <Script
+        src="https://cdn.datatables.net/2.3.2/js/dataTables.js"
+        strategy="afterInteractive"
+        onLoad={handleScriptLoad}
+      />
     </>
   )
 }
